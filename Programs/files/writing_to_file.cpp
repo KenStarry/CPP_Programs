a5c0adb61cdbb1fe89c8321c84a0439b501eb08c
KenStarry <starrycodes@gmail.com>
2023-03-06 20:27:57 +0300

    Created a file

--- Programs/files/writing_to_file.cpp
+++ Programs/files/writing_to_file.cpp
@@ -1,3 +1,18 @@
 //
 // Created by kenne on 3/6/2023.
 //
+#include <iostream>
+#include <fstream>
+
+int main() {
+
+    std::ofstream fout;
+
+    fout.open(R"(S:\Programming\C++\Programs\files\my files\greetings.txt)", std::ios::out);
+
+    fout << "Sheilla is such a beautiful, lovely lady...however, a huge drama queen.";
+
+    fout.close();
+
+    return 0;
+}
