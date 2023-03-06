88f77711f8be80d18892c3ba1aa153c88c05c4b6
KenStarry <starrycodes@gmail.com>
2023-03-06 20:06:52 +0300

    vectors

--- Programs/vectors/inserting.cpp
+++ Programs/vectors/inserting.cpp
@@ -1,3 +1,28 @@
 //
 // Created by kenne on 2/20/2023.
 //
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+    vector<float> nums;
+    nums.resize(10);
+
+    for (int i = 0; i < 5; i++) {
+
+        cout << "Enter a number : ";
+        cin >> nums[i];
+    }
+
+    cout << "Before insertion : " << endl;
+    for (float i: nums) {
+        cout << i << "\t";
+    }
+
+    nums.insert(nums.begin(), 23);
+
+    return 0;
+}
