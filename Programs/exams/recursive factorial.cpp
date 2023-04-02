f7f1b00a98df727dd03cc80f65d79a16cc34d348
KenStarry <starrycodes@gmail.com>
2023-04-02 21:28:58 +0300

    factorial

--- Programs/exams/recursive factorial.cpp
+++ Programs/exams/recursive factorial.cpp
@@ -1,3 +1,30 @@
 //
 // Created by kenne on 4/2/2023.
 //
+#include <iostream>
+
+using namespace std;
+
+int factorial(int n = 5) {
+    if (n == 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+int main() {
+
+    int n;
+
+    cout << "Factorial of the first :";
+    cin >> n;
+
+    if (n == 0) {
+        cout << endl << factorial();
+    } else {
+        cout << endl << factorial(n);
+    }
+
+    return 0;
+}
